663473de0d3bb6be5b7ae1ecc203786adf3f07a4
Alejandro Farfan <alejofarfan@gmail.com>
2026-03-16 20:43:20 +0000

    File changes

--- src/components/routemap/RouteMapView.jsx
+++ src/components/routemap/RouteMapView.jsx
@@ -80,8 +80,8 @@ export default function RouteMapView({ visits, clientsMap, selectedId, onSelectV
       className="z-0"
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
       />
 
       {markers.length > 0 && <FitBounds positions={polylinePositions} />}
